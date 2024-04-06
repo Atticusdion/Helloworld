875cc81e3f010ede35503ffecf5b481f6b066072
Atticus69 <atishsharma32@outlook.com>
2024-04-06 09:19:34 -0500

    slight update

--- Annotation/annotation_test.py
+++ Annotation/annotation_test.py
@@ -3,23 +3,25 @@ path = 'Annotation/coding_qual_input.txt'
 def decode(message_file):
 
     step = 1
-    pos = 1
+    position = 1
     code = ""
 
     with open(message_file, 'r') as file:
         
         lines = file.readlines()
-        len(lines)
 
-        while pos <= len(lines):
+        while position <= len(lines):
+            
             for line in lines:   
                 words = line.strip().split()
 
-                if words and int(words[0]) == pos:
+                if words and int(words[0]) == position:
                     code = code + words[1] + " "
-            step += 1
-            pos += step
 
+            step += 1
+            position += step
+
+    code = code.strip()
     return code
 
 print(f"The decoded text is '{decode(path)}'")
